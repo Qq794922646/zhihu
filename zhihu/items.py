475864d675d772ecbf0a5727cc50369ed7d2cdb6
Administrator <794922646@qq.com>
2019-04-18 18:08:30 +0800

    爬虫知乎的问题

--- zhihu/items.py
+++ zhihu/items.py
@@ -12,3 +12,25 @@ class ZhihuItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+class ZhihuQuestionItem(scrapy.Item):
+    ID=scrapy.Field()
+    topics=scrapy.Field()
+    url=scrapy.Field()
+    title=scrapy.Field()
+    content=scrapy.Field()
+    answer_num=scrapy.Field()
+    comments_num=scrapy.Field()
+    watch_user_num=scrapy.Field()
+    click_num=scrapy.Field()
+    crawl_time=scrapy.Field()
+class ZhihuAnswerItem(scrapy.Item):
+    ID = scrapy.Field()
+    url = scrapy.Field()
+    quertion_id= scrapy.Field()
+    author_id= scrapy.Field()
+    content= scrapy.Field()
+    parise_num= scrapy.Field()
+    comments_num= scrapy.Field()
+    create_time= scrapy.Field()
+    update_time= scrapy.Field()
+    crawl_time= scrapy.Field()
--- zhihu/spiders/zhihu.py
+++ zhihu/spiders/zhihu.py
@@ -1,17 +1,46 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from selenium import webdriver
+import re
+from scrapy.loader import ItemLoader
+from zhihu.items import ZhihuAnswerItem,ZhihuQuestionItem
 import pickle
 import time
-from selenium.webdriver.common.keys import Keys
+from urllib import parse
 from mouse import move,click
 class ZhihuSpider(scrapy.Spider):
     name = 'zhihu'
     allowed_domains = ['www.zhihu.com']
     start_urls = ['https://www.zhihu.com/']
-
+    # headers='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
     def parse(self, response):
+        # 提取出HTML页面中的所有url，并跟踪这些url
+        all_urls=response.css("a::attr(href)").extract()
+        all_urls=[parse.urljoin(response.url,url)for url in all_urls]
+        # all_urls=filter(lambda x:True if x.startwith("https") else False,all_urls)
+        all_urls = filter(lambda x: True if x.startswith("https") else False, all_urls)
+        for url in all_urls:
+            match_obj=re.match('(.*zhihu.com/question/(\d+))(/|$).*',url)
+            if match_obj:
+                request_url=match_obj.group(1)
+                quetion_id=match_obj.group(2)
+                yield scrapy.Request(request_url,callback=self.parse_question)
+    # 从页面提取question item
+    def parse_question(self, response):
+        match_obj = re.match('(.*zhihu.com/question/(\d+))(/|$).*', response.url)
+        if match_obj:
+            quetion_id = int(match_obj.group(2))
+        item_loader=ItemLoader(item=ZhihuQuestionItem(),response=response)
+        item_loader.add_css('title','.QuestionHeader-title::text')
+        item_loader.add_css('content', '.QuestionHeader-detail')
+        item_loader.add_value('url', response.url)
+        item_loader.add_value('ID',quetion_id)
+        item_loader.add_css('answer_num','.List-headerText span::text')
+        item_loader.add_css('comments_num','.QuestionHeader-Comment button::text')
+        item_loader.add_css('watch_user_num','.NumberBoard-itemValue::text')
+        item_loader.add_css('topics','.QuestionHeader-topics .Popover div::text')
+        quetion_item=item_loader.load_item()
         pass
+
     # def start_requests(self):
     #
     #     # cookies=pickle.load(open('D:/python/project/zhihu/cookies/zhihui.cookie',"rb"))
@@ -50,6 +79,13 @@ class ZhihuSpider(scrapy.Spider):
     #
     #     pass
     def start_requests(self):
+
+        cookies=pickle.load(open('D:/python/project/zhihu/cookies/zhihui.cookie',"rb"))
+        cookie_dict = {}
+        for cookie in cookies:
+            cookie_dict[cookie["name"]] = cookie["value"]
+        return [scrapy.Request(url=self.start_urls[0], dont_filter=True, cookies=cookie_dict)]
+
         from selenium import webdriver
         from selenium.webdriver.chrome.options import Options
         from selenium.webdriver.common.keys import Keys
@@ -177,6 +213,20 @@ class ZhihuSpider(scrapy.Spider):
                 brower.find_element_by_css_selector(".SignFlow-password input").send_keys(Keys.CONTROL + 'a')
                 # 在输入密码
                 brower.find_element_by_css_selector(".SignFlow-password input").send_keys("q143256987")
-                move(898, 617)
+                move(933, 641)
                 click()
-        time.sleep(60)
+        time.sleep(10)
+        if brower.find_element_by_css_selector(".Tabs-link is-active"):
+            print('进入首页')
+            click()
+        else:
+            print('进入首页失败')
+
+
+        brower.get('https://www.zhihu.com/')
+        cookies = brower.get_cookies()
+        pickle.dump(cookies,open('D:/python/project/zhihu/cookies/zhihui.cookie',"wb"))
+        cookie_dict={}
+        for cookie in cookies:
+            cookie_dict[cookie["name"]]=cookie["value"]
+        return [scrapy.Request(url=self.start_urls[0],dont_filter=True,cookies=cookie_dict)]
